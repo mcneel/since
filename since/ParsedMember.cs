3bcc73f741070633ed0b76f3b25053e83462aa9b
Steve Baer <steve@mcneel.com>
2020-03-09 08:43:24 -0700

    Initial commit

---
+++ since/ParsedMember.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace since
+{
+  class ParsedMember
+  {
+    public ParsedMember(MemberDeclarationSyntax member, DocumentationCommentTriviaSyntax documentation)
+    {
+      Member = member;
+      Documentation = documentation;
+    }
+    public DocumentationCommentTriviaSyntax Documentation { get; }
+    private MemberDeclarationSyntax Member { get; }
+
+    public bool HasSinceTag()
+    {
+      bool exists = (Documentation != null && Documentation.Content.ToFullString().Contains("<since>"));
+      return exists;
+    }
+
+    public int SinceInsertIndex()
+    {
+      int rc = Member.SpanStart;
+      return rc;
+    }
+
+    public string Signature
+    {
+      get
+      {
+        string className = "";
+        string ns = "";
+        var parent = Member.Parent.Parent;
+        while(parent != null)
+        {
+          var parentClassDeclaration = parent as ClassDeclarationSyntax;
+          if(parentClassDeclaration!=null)
+          {
+            ns = $"{parentClassDeclaration.Identifier}.{ns}";
+          }
+          var namespaceDeclaration = parent as NamespaceDeclarationSyntax;
+          if(namespaceDeclaration!=null)
+          {
+            ns = $"{namespaceDeclaration.Name}.{ns}";
+          }
+          parent = parent.Parent;
+        }
+        if (ns.Length>0)
+          className = ns;
+        var classDeclaration = Member.Parent as ClassDeclarationSyntax;
+        if( classDeclaration!=null)
+        {
+          className = $"{className}{classDeclaration.Identifier}";
+        }
+        var interfaceDeclaration = Member.Parent as InterfaceDeclarationSyntax;
+        if( interfaceDeclaration != null )
+        {
+          className = $"{className}{interfaceDeclaration.Identifier}";
+        }
+        var structDeclaration = Member.Parent as StructDeclarationSyntax;
+        if( structDeclaration != null)
+        {
+          className = $"{className}{structDeclaration.Identifier}";
+        }
+        if (classDeclaration == null && interfaceDeclaration == null && structDeclaration == null)
+          throw new System.NotImplementedException();
+
+        MethodDeclarationSyntax method = Member as MethodDeclarationSyntax;
+        if( method != null)
+        {
+          var signature = new System.Text.StringBuilder();
+          signature.Append($"{className}.{method.Identifier}(");
+          int parameterCount = method.ParameterList.Parameters.Count;
+          for (int i = 0; i < parameterCount; i++)
+          {
+            if (i > 0)
+              signature.Append(",");
+            var parameter = method.ParameterList.Parameters[i];
+            string paramType = parameter.Type.ToString();
+            int angleIndex = paramType.IndexOf('<');
+            string prefixType = "";
+            if(angleIndex>0)
+            {
+              prefixType = paramType.Substring(0, angleIndex);
+              int prefixIndex = prefixType.LastIndexOf('.');
+              if (prefixIndex > 0)
+                prefixType = prefixType.Substring(prefixIndex + 1);
+              string genericType = paramType.Substring(angleIndex + 1);
+              int genericIndex = genericType.LastIndexOf('.');
+              if (genericIndex > 0)
+                genericType = genericType.Substring(genericIndex + 1);
+              paramType = prefixType + "<" + genericType;
+            }
+            else
+            {
+              int index = paramType.LastIndexOf('.');
+              if (index > 0)
+                paramType = paramType.Substring(index + 1);
+            }
+            signature.Append(paramType);
+          }
+          signature.Append(")");
+          return signature.ToString();
+        }
+
+        PropertyDeclarationSyntax property = Member as PropertyDeclarationSyntax;
+        if (property != null)
+        {
+          var signature = new System.Text.StringBuilder();
+          signature.Append($"{className}.{property.Identifier}");
+          return signature.ToString();
+        }
+
+        EventDeclarationSyntax evt = Member as EventDeclarationSyntax;
+        if (evt != null)
+        {
+          var signature = new System.Text.StringBuilder();
+          signature.Append($"{className}.{evt.Identifier}");
+          return signature.ToString();
+        }
+
+        OperatorDeclarationSyntax op = Member as OperatorDeclarationSyntax;
+        if(op != null)
+        {
+          var signature = new System.Text.StringBuilder();
+          signature.Append($"{className}.{op.OperatorToken}");
+          return signature.ToString();
+        }
+
+        EventFieldDeclarationSyntax eventField = Member as EventFieldDeclarationSyntax;
+        if(eventField != null)
+        {
+          var signature = new System.Text.StringBuilder();
+          string declaration = eventField.ToString();
+          int index = declaration.LastIndexOf(' ');
+          declaration = declaration.Substring(index + 1, declaration.Length - 1 - (index + 1));
+          signature.Append($"{className}.{declaration}");
+          return signature.ToString();
+        }
+        throw new System.NotImplementedException();
+      }
+    }
+  }
+}
---
+++ since/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace since
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      const string pathOld = @"C:\dev\rhinocommon_version\5.11\RhinoCommon.dll";// @"C:\Program Files\Rhino 6\System\RhinoCommon.dll";
+      const string pathNew = @"C:\dev\rhinocommon_version\5.12\RhinoCommon.dll";//@"C:\Program Files\Rhino 7 WIP\System\RhinoCommon.dll";
+      const string sinceVersion = "5.12";
+      var newMembersTask = NewMembersAsync(pathOld, pathNew);
+
+      int countFoundInSource = 0;
+      string rhinocommonDirectory = @"C:\dev\github\mcneel\rhino\src4\DotNetSDK\rhinocommon\dotnet\";
+      foreach(var sourceFile in AllSourceFiles(rhinocommonDirectory))
+      {
+        bool modified = false;
+        //Console.WriteLine($"parse: {sourceFile}");
+        string text = System.IO.File.ReadAllText(sourceFile);
+        var parsedItems = SourceFileWalker.ParseSource(text);
+        // Reverse so we walk backward through the source. This will allow us
+        // to use proper character offets as we insert strings
+        parsedItems.Reverse();
+        var newMembers = newMembersTask.Result;
+        foreach(var parsedItem in parsedItems)
+        {
+          string signature = parsedItem.Signature.ToLower();
+          if (!newMembers.ContainsKey(signature))
+            continue;
+          newMembers[signature] = true;
+          countFoundInSource++;
+
+          if( !parsedItem.HasSinceTag() )
+          {
+            int insertIndex = parsedItem.SinceInsertIndex();
+            StringBuilder since = new StringBuilder();
+            while(text[insertIndex-1]==' ')
+            {
+              since.Append(" ");
+              insertIndex--;
+            }
+            since.AppendLine($"/// <since>{sinceVersion}</since>");
+            text = text.Insert(insertIndex, since.ToString());
+            modified = true;
+          }
+        }
+        if (modified)
+          System.IO.File.WriteAllText(sourceFile, text);
+      }
+
+      foreach(var kv in newMembersTask.Result)
+      {
+        if( kv.Value == false)
+        {
+          Console.WriteLine($"Missed {kv.Key}");
+        }
+      }
+
+      Console.WriteLine($"{newMembersTask.Result.Keys.Count} new items");
+      Console.WriteLine($"{countFoundInSource} found in source");
+
+      Console.ReadKey();
+    }
+
+    static IEnumerable<string> AllSourceFiles(string sourcePath)
+    {
+      foreach (string file in System.IO.Directory.EnumerateFiles(sourcePath, "*.cs", System.IO.SearchOption.AllDirectories))
+      {
+        string filename = System.IO.Path.GetFileName(file);
+        if (filename.Equals(".cs") || filename.StartsWith("AutoNative"))
+          continue;
+        if (file.Contains("\\obj\\"))
+          continue;
+        yield return file;
+      }
+    }
+
+    static Task<Dictionary<string,bool>> NewMembersAsync(string oldAssemblyPath, string newAssemblyPath)
+    {
+      return Task.Run(() =>
+      {
+        var oldDict = AssemblyItems(oldAssemblyPath);
+        var newDict = AssemblyItems(newAssemblyPath);
+
+        Dictionary<string, bool> newItems = new Dictionary<string, bool>();
+        foreach (var key in newDict.Keys)
+        {
+          if (!oldDict.ContainsKey(key))
+            newItems[key] = false;
+        }
+        return newItems;
+      });
+    }
+
+    static string TweakTypeName(string name)
+    {
+      bool isArray = name.EndsWith("[]");
+      if (isArray)
+        name = name.Substring(0, name.Length - 2);
+      if (name.Equals("Boolean"))
+        name = "bool";
+      if (name.Equals("Int32"))
+        name = "int";
+      if (name.Equals("UInt32"))
+        name = "uint";
+      if (name.Equals("Single"))
+        name = "float";
+      if (name.Equals("Int16"))
+        name = "short";
+      if (name.Equals("UInt16"))
+        name = "ushort";
+      if (isArray)
+        name += "[]";
+
+      return name;
+    }
+
+    static Dictionary<string, bool> AssemblyItems(string path)
+    {
+      Dictionary<string, bool> items = new Dictionary<string, bool>();
+      if (string.IsNullOrEmpty(path))
+        return items;
+      System.Reflection.Assembly assembly = System.Reflection.Assembly.LoadFrom(path);
+      Type[] types = assembly.GetExportedTypes();
+      foreach(var type in types)
+      {
+        var methods = type.GetMethods();
+        foreach(var method in methods)
+        {
+          if (method.DeclaringType != type)
+            continue;
+          StringBuilder signature = new StringBuilder($"{type.FullName}.{method.Name}");
+          if (method.IsSpecialName)
+          {
+            string name = method.Name;
+            if (name.StartsWith("get_") || name.StartsWith("set_") || name.StartsWith("add_"))
+              signature = new StringBuilder($"{type.FullName}.{name.Substring(4)}");
+            if (name.StartsWith("remove_"))
+              signature = new StringBuilder($"{type.FullName}.{name.Substring(7)}");
+            if (name.StartsWith("op_"))
+            {
+              switch(name)
+              {
+                case "op_Equality":
+                  name = "==";
+                  break;
+                case "op_Inequality":
+                  name = "!=";
+                  break;
+                case "op_Addition":
+                  name = "+";
+                  break;
+                case "op_Subtraction":
+                  name = "-";
+                  break;
+                case "op_LessThan":
+                  name = "<";
+                  break;
+                case "op_GreaterThan":
+                  name = ">";
+                  break;
+                case "op_Multiply":
+                  name = "*";
+                  break;
+                case "op_Division":
+                  name = "/";
+                  break;
+                case "op_LessThanOrEqual":
+                  name = "<=";
+                  break;
+                case "op_GreaterThanOrEqual":
+                  name = ">=";
+                  break;
+                default:
+                  break;
+              }
+              signature = new StringBuilder($"{type.FullName}.{name}");
+            }
+          }
+          else
+          {
+            signature.Append("(");
+            var parameters = method.GetParameters();
+            if( parameters.Length == 0)
+            {
+              if (method.Name.Equals("GetHashCode") || method.Name.Equals("ToString"))
+                continue;
+            }
+            if( parameters.Length == 1 && method.Name.Equals("Equals"))
+            {
+              if (parameters[0].ParameterType.Name.Equals("Object"))
+                continue;
+            }
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+              if (i > 0)
+                signature.Append(",");
+              var ptype = parameters[i].ParameterType;
+              string name = ptype.Name.Replace("&","");
+              if (ptype.IsGenericType && ptype.GenericTypeArguments.Length > 0)
+              //              if (name.Equals("IEnumerable`1"))
+              {
+                int index = name.IndexOf("`");
+
+                name = TweakTypeName(name.Substring(0, index)) + "<";
+                for (int j = 0; j < ptype.GenericTypeArguments.Length; j++)
+                {
+                  if (j > 0)
+                    name += ",";
+                  name += TweakTypeName(ptype.GenericTypeArguments[j].Name);
+                }
+                name += ">";
+              }
+              else
+                name = TweakTypeName(name);
+
+              signature.Append(name);
+            }
+            signature.Append(")");
+          }
+
+          string key = signature.ToString().ToLower().Replace("+",".");
+          items[key] = false;
+        }
+      }
+      return items;
+    }
+  }
+}
