7a93926c60c423f6d58f58d63772863c5eb8e47d
Steve Baer <steve@mcneel.com>
2021-04-18 18:31:07 -0700

    add 7.1 through 7.5 assemblies

--- since/Program.cs
+++ since/Program.cs
@@ -13,8 +13,8 @@ namespace since
         {
             //string[] directories = Directory.GetDirectories(@"..\..\..\rhinocommon_versions\6");
 
-            const string pathOld = @"..\..\..\rhinocommon_versions\6.29\RhinoCommon.dll";
-            const string sinceVersion = "7.0";
+            const string pathOld = @"..\..\..\rhinocommon_versions\7.4\RhinoCommon.dll";
+            const string sinceVersion = "7.5";
             string pathNew = $"..\\..\\..\\rhinocommon_versions\\{sinceVersion}\\RhinoCommon.dll";
             var modifiedMembersTask = ModifiedMembersAsync(pathOld, pathNew);
             bool fileWritten = false;
